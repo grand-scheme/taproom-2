09ab3be5cfd2cba1e72bbd37b1d51470f6066b08
Shannon Grantski <grantski@pm.me>
2021-02-12 11:00:52 -0800

    create static list for pre-state inventory

---
+++ src/data/kegs.js
@@ -0,0 +1,43 @@
+// until databasing / Live State is involved, this is a hard-coded "database" of kegs for the taproom //
+export const kegsList = [
+  { 
+    kegId: 1, 
+    name: "Alpha", 
+    brandName: "Get Kegged", 
+    price: 4.5, 
+    abv: 4.5, 
+    inventory: 100
+  },
+  { 
+    kegId: 2, 
+    name: "Beta", 
+    brandName: "Get Kegged", 
+    price: 7, 
+    abv: 14, 
+    inventory: 90
+  },
+  { 
+    kegId: 3, 
+    name: "Gamma", 
+    brandName: "Get Kegged", 
+    price: 9, 
+    abv: 12, 
+    inventory: 55
+  },
+  { 
+    kegId: 4, 
+    name: "Delta", 
+    brandName: "Beveragy Hills", 
+    price: 9, 
+    abv: 10.5, 
+    inventory: 32
+  },
+  { 
+    kegId: 5, 
+    name: "Epsilon", 
+    brandName: "Beveragy Hills", 
+    price: 6.5, 
+    abv: 6, 
+    inventory: 12
+  }
+];
