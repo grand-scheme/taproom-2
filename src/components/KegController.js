9fc067b42caa6ce29ffc75c3c9536556ed47078e
Shannon Grantski <grantski@pm.me>
2021-02-12 15:57:35 -0800

    add functionality for click-state changes

--- src/components/KegController.js
+++ src/components/KegController.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import AddNewKeg from './AddNewKeg';
 import KegList from './KegList';
+import KegDetail from './KegDetail';
 
 class KegController extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
       visibleNewKegForm: false,
-      OO_fullKegList: []
+      OO_fullKegList: [],
+      OO_selectedKeg: null
     };
   }
 
   handleClick = () => {
-    this.setState(prevState => ({
-      visibleNewKegForm: !prevState.visibleNewKegForm
-    }));
+    if (this.state.OO_selectedKeg != null) {
+      this.setState({
+        visibleNewKegForm: false,
+        OO_selectedKeg: null
+      });
+    } else {
+      this.setState(prevState => ({
+        visibleNewKegForm: !prevState.visibleNewKegForm
+      }));
+    }
   }
 
   OO_handleAddingKegToList = (OO_newKeg) => {
@@ -25,17 +34,23 @@ class KegController extends React.Component{
     });
   }
 
+  OO_handleChangeSelectedKeg = (id) => {
+    const OO_selectedKeg = this.state.OO_fullKegList.filter(OO_keg => OO_keg.id === id)[0];
+    this.setState({OO_selectedKeg: OO_selectedKeg});
+  }
+
   render(){
     let currentVisibleState = null;
     let addKegBtnText = null;
-    if (this.state.visibleNewKegForm) {
+
+    if (this.state.OO_selectedKeg !=null) {
+      currentVisibleState = <KegDetail OO_keg = {this.state.OO_selectedKeg} />
+      addKegBtnText = "View Kegs on Tap";
+    } else if (this.state.visibleNewKegForm) {
       currentVisibleState = <AddNewKeg OO_onNewKegCreation={this.OO_handleAddingKegToList} />
       addKegBtnText = "View Kegs on Tap"
     } else {
-      currentVisibleState = 
-      <KegList 
-        OO_kegList={this.state.OO_fullKegList} 
-      />
+      currentVisibleState = <KegList OO_kegList={this.state.OO_fullKegList} OO_onKegSelection={this.OO_handleChangeSelectedKeg} />
       addKegBtnText = "Add Keg to Inventory"
         }
     return(
