c1888dadc1d0fe00036f695cb9a873bbe93340ce
Shannon Grantski <grantski@pm.me>
2021-02-12 11:47:23 -0800

    map proptypes in keg list

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Keg from './Keg';
+import {kegsList} from '../data/kegs'
 
 function KegList(){
   return(
     <>
       <h1>KegList placeholder</h1>
-      <Keg />
+      {kegsList.map((keg, index) =>
+      <Keg 
+          kegId={keg.kegId}
+          name={keg.name}
+          brandName={keg.brandName}
+          price={keg.price}
+          abv={keg.abv}
+          inventory={keg.inventory}
+          key={index}
+        />
+      )}
     </>
   );
 }
