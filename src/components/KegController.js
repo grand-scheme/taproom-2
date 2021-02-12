76656d3378847327d5e973bacb14fe8f331c8c12
Shannon Grantski <grantski@pm.me>
2021-02-12 12:03:01 -0800

    add base KegController.js

---
+++ src/components/KegController.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import AddNewKeg from './AddNewKeg';
+import KegList from './KegList';
+
+class KegController extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render(){
+    return(
+      <>
+        <h1>KegController placeholder</h1>
+      </>
+    );
+  }
+}
+
+export default KegController;
