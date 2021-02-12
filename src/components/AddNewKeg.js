fcfdb1110940e1bd4c25f645d14ba0bac47fa1ab
Shannon Grantski <grantski@pm.me>
2021-02-12 12:59:07 -0800

    import v4(), add submit form

--- src/components/AddNewKeg.js
+++ src/components/AddNewKeg.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { v4 } from 'uuid' ;
 
 function AddNewKeg(){
+  function handleNewKegSubmission(e){
+    e.preventDefault();
+    console.log(e.target.kegId.value);
+    console.log(e.target.name.value);
+    console.log(e.target.brandName.value);
+    console.log(e.target.price.value);
+    console.log(e.target.abv.value);
+    console.log(e.target.inventory.value);
+  }
+
   return(
     <>
       <h1>AddNewKeg placeholder</h1>
+      <form onSubmit={handleNewKegSubmission}>
+        {/* kegId={v4} */}
+        <input 
+          type='hidden'
+          name="kegId"
+          value= {v4()}
+        />
+        <input 
+          type='text'
+          name="name"
+          placeholder="Brew Name"
+        />
+        <input 
+          type='text'
+          name="brandName"
+          placeholder="Brand Name"
+        />
+        <input 
+          type='text'
+          name="price"
+          placeholder="Price per Pint"
+        />
+        <input 
+          type='text'
+          name="abv"
+          placeholder="Alcohol by Volume"
+        />
+        <input 
+          type='text'
+          name="inventory"
+          placeholder="Pints In Keg"
+        />
+        <button type="submit"> Add </button>
+      </form>
     </>
   );
 }
