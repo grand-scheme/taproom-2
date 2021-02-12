eeb5a1a5b0b6211833b3550caf72c086dfafde5a
Shannon Grantski <grantski@pm.me>
2021-02-12 14:09:12 -0800

    add temp list for State adds

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Keg from './Keg';
 import {kegsList} from '../data/kegs'
+import PropTypes from "prop-types";
 
-function KegList(){
+function KegList(props){
   return(
     <>
       {kegsList.map((keg) =>
@@ -16,9 +17,25 @@ function KegList(){
           key={keg.kegId}
         />
         )}
+
+        {props.OO_kegList.map((OO_keg, index) =>
+          <Keg
+            kegId={OO_keg.kegId}
+            name={OO_keg.name}
+            brandName={OO_keg.brandName}
+            price={OO_keg.price}
+            abv={OO_keg.abv}
+            inventory={OO_keg.inventory}
+            key={index}
+          />
+        )}
       <hr />
     </>
   );
 }
 
+// Add propTypes for OO_kegList
+KegList.propTypes = {
+  OO_kegList: PropTypes.array
+};
 export default KegList;
