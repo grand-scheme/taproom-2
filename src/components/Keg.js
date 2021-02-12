ef8e5e22011b4c302e4ac2cb1786b230b7a8ea1b
Shannon Grantski <grantski@pm.me>
2021-02-12 11:47:01 -0800

    add proptypes to keg

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import {kegsList} from '../data/kegs'
+import PropTypes from 'prop-types';
 
-function Keg(){
+function Keg(props){
   return(
     <>
       <h1>Keg placeholder</h1>
+      <h2>{props.name}</h2>
     </>
   );
 }
+
+Keg.propTypes = {
+  kegId: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  brandName: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  abv: PropTypes.number.isRequired,
+  inventory: PropTypes.number.isRequired
+};
 
 export default Keg;
