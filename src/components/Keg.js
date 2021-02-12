93f7fb7c6d45511954309bd2636898d9b62ff335
Shannon Grantski <grantski@pm.me>
2021-02-12 15:57:13 -0800

    add more display properties

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 function Keg(props){
   return(
     <>
-      <hr />
-      <h2>{props.name}</h2>
+      <div onClick = {() => props.OO_whenClickKeg(props.id)}>
+        <h2>{props.name} - {props.brandName}</h2>
+        <p>{props.abv}</p>
+      </div>
     </>
   );
 }
 
 Keg.propTypes = {
-  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   brandName: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   abv: PropTypes.number.isRequired,
-  inventory: PropTypes.number.isRequired
+  inventory: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  OO_whenClickKeg: PropTypes.func
 };
 
 export default Keg;
