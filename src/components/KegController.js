1e797c9c73d85fcc27b5d72ba23f214a37b5cc0b
Shannon Grantski <grantski@pm.me>
2021-02-12 14:10:05 -0800

    add an array to hold Added Kegs

--- src/components/KegController.js
+++ src/components/KegController.js
@@ -6,7 +6,8 @@ class KegController extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      visibleNewKegForm: false
+      visibleNewKegForm: false,
+      OO_fullKegList: []
     };
   }
 
@@ -16,14 +17,25 @@ class KegController extends React.Component{
     }));
   }
 
+  OO_handleAddingKegToList = (OO_newKeg) => {
+    const OO_newFullKegList = this.state.OO_fullKegList.concat(OO_newKeg);
+    this.setState({
+      OO_fullKegList: OO_newFullKegList,
+      visibleNewKegForm: false
+    });
+  }
+
   render(){
     let currentVisibleState = null;
     let addKegBtnText = null;
     if (this.state.visibleNewKegForm) {
-      currentVisibleState = <AddNewKeg />
+      currentVisibleState = <AddNewKeg OO_onNewKegCreation={this.OO_handleAddingKegToList} />
       addKegBtnText = "View Kegs on Tap"
     } else {
-      currentVisibleState = <KegList />
+      currentVisibleState = 
+      <KegList 
+        OO_kegList={this.state.OO_fullKegList} 
+      />
       addKegBtnText = "Add Keg to Inventory"
         }
     return(
