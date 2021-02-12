b427f5ee40bd0c8e85b0775f6e663a10860cac59
Shannon Grantski <grantski@pm.me>
2021-02-12 11:27:34 -0800

    add import for keglist, keg

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import Header from './Header';
+import KegList from './KegList';
 
 function App() {
   return (
     <>
       <Nav />
       <Header />
+      <KegList />
     </>
     );
 }
---
+++ src/components/KegList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Keg from './Keg';
+
+function KegList(){
+  return(
+    <>
+      <h1>KegList placeholder</h1>
+      <Keg />
+    </>
+  );
+}
+
+export default KegList;
