7a584be10955eb0c707002aad01ac1bd6baa8d28
Shannon Grantski <grantski@pm.me>
2021-02-13 01:34:04 -0800

    remove unused code for static data

--- src/components/KegList.js
+++ src/components/KegList.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import Keg from './Keg';
-// import {kegsList} from '../data/kegs'
 import PropTypes from "prop-types";
 
 function KegList(props){
   return(
     <>
-      {/* {kegsList.map((keg) =>
-      <Keg 
-          OO_whenClickKeg = { props.OO_onKegSelection }
-          id={keg.id}
-          name={keg.name}
-          brandName={keg.brandName}
-          price={keg.price}
-          abv={keg.abv}
-          inventory={keg.inventory}
-          key={keg.id}
-        />
-        )} */}
-
-        {props.OO_kegList.map((OO_keg) =>
-          <Keg
+      {props.OO_kegList.map((OO_keg) =>
+        <Keg
           OO_whenClickKeg = { props.OO_onKegSelection }
           id={OO_keg.id}
           name={OO_keg.name}
