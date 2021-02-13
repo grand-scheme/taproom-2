bf960eae0e6308efcbfba9d2b434c3bdd82706de
Shannon Grantski <grantski@pm.me>
2021-02-12 16:46:49 -0800

    add delete button

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,18 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function KegDetail(props){
-  const { OO_keg } = props;
+  const { OO_keg, onClickingDelete } = props;
+  
   return(
     <>
       <h1>placeholder for details</h1>
       <h2>{OO_keg.name} - {OO_keg.brandName}</h2>
         <p>{OO_keg.abv}</p>
+
+        <button onClick={()=> onClickingDelete(OO_keg.id) }>
+          Delete Keg
+        </button>
     </>
   );
 }
 
 KegDetail.propTypes = {
-  OO_keg: PropTypes.object
+  OO_keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default KegDetail;
