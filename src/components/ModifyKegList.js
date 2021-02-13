17b18a508a6e4436361f0549e008bc25946e18f3
Shannon Grantski <grantski@pm.me>
2021-02-12 17:31:34 -0800

    add prelimiary Modify Keg draft .js

--- src/components/ModifyKegList.js
+++ src/components/ModifyKegList.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from "prop-types";
+import Form from "./Form";
 
-function ModifyKegList(){
+function ModifyKegList(props){
+  function handleEditKegSubmission(e){
+    e.preventDefault();
+    props.OO_onKegModification({
+      name: e.target.name.value,
+      brandName: e.target.brandName.value,
+      price: parseInt(e.target.price.value),
+      abv: parseInt(e.target.abv.value),
+      inventory: parseInt(e.target.inventory.value),
+      id: e.target.id.value
+    });
+  }
   return(
     <>
       <h1>ModifyKegList placeholder</h1>
+      <Form 
+        formSubmissionHandler={handleEditKegSubmission} buttonText="Modify Keg Details"
+      />
     </>
   );
 }
+
+ModifyKegList.propTypes = {
+  OO_onKegModification: PropTypes.func
+};
 
 export default ModifyKegList;
