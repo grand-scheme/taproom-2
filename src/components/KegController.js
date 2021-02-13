ddf1f58433d2c4fce63f21622147aa72a3fa929e
Shannon Grantski <grantski@pm.me>
2021-02-12 18:48:06 -0800

    add edit functionality

--- src/components/KegController.js
+++ src/components/KegController.js
@@ -2,6 +2,7 @@ import React from 'react';
 import AddNewKeg from './AddNewKeg';
 import KegList from './KegList';
 import KegDetail from './KegDetail';
+import ModifyKegList from './ModifyKegList';
 
 class KegController extends React.Component{
   constructor(props) {
@@ -17,6 +18,7 @@ class KegController extends React.Component{
   handleClick = () => {
     if (this.state.OO_selectedKeg != null) {
       this.setState({
+        visibleEditKegForm: false,
         visibleNewKegForm: false,
         OO_selectedKeg: null
       });
@@ -25,6 +27,10 @@ class KegController extends React.Component{
         visibleNewKegForm: !prevState.visibleNewKegForm
       }));
     }
+  }
+
+  handleEditClick = () => {
+    this.setState({visibleEditKegForm: true});
   }
 
   OO_handleAddingKegToList = (OO_newKeg) => {
@@ -50,6 +56,17 @@ class KegController extends React.Component{
     });
   }
 
+  handleEditKeg = (kegToModify) => {
+    const modifiedKegList = this.state.OO_fullKegList
+      .filter(keg => keg.id !== this.state.OO_selectedKeg.id)
+      .concat(kegToModify);
+    this.setState({
+      OO_fullKegList: modifiedKegList,
+      visibleEditKegForm: false,
+      OO_selectedKeg: null
+    });
+  }
+
 
 
 
@@ -58,8 +75,19 @@ class KegController extends React.Component{
     let currentVisibleState = null;
     let addKegBtnText = null;
 
-    if (this.state.OO_selectedKeg !=null) {
-      currentVisibleState = <KegDetail OO_keg = {this.state.OO_selectedKeg} onClickingDelete = {this.handleDeletingKeg} />
+    if (this.state.visibleEditKegForm) {
+      currentVisibleState = <ModifyKegList
+        keg = {this.state.OO_selectedKeg}
+        OO_onKegModification = {this.handleEditKeg}
+        />
+        addKegBtnText = "View Kegs on Tap"
+    } else if (this.state.OO_selectedKeg !=null) {
+      currentVisibleState = 
+      <KegDetail 
+        OO_keg = {this.state.OO_selectedKeg} 
+        onClickingDelete = {this.handleDeletingKeg}
+        onClickingEdit = {this.handleEditClick}
+      />
       addKegBtnText = "View Kegs on Tap";
     } else if (this.state.visibleNewKegForm) {
       currentVisibleState = <AddNewKeg OO_onNewKegCreation={this.OO_handleAddingKegToList} />
