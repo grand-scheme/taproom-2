7b40a7e2566dd431d09dee6df718352c55a17d9a
Shannon Grantski <grantski@pm.me>
2021-02-13 03:59:07 -0800

    add in/out of stockage message

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -4,11 +4,28 @@ import PropTypes from "prop-types";
 function KegDetail(props){
   const { keg, onClickingDelete, onClickingEdit } = props;
   
+  let stockMessage = null;
+  const inStock = "Approximately " + keg.inventory + " pints available.";
+  const lowStock = inStock + " Almost Empty!";
+  const outOfStock = "Out of Stock!";
+
+  if (keg.inventory === 0) {
+    stockMessage = outOfStock;
+  } else if (keg.inventory < 10 ) {
+    stockMessage = lowStock;
+  } else {
+    stockMessage = inStock;
+  }
+
   return(
     <>
-      <h1>placeholder for details</h1>
-      <h2>{keg.name} - {keg.brandName}</h2>
-        <p>{keg.abv}</p>
+      <h1>{keg.name}</h1>
+      <h2>Proudly brewed by {keg.brandName}</h2>
+        <p>
+          {keg.abv}% Alcohol by Volume <br />
+          $ {keg.price} per pint <br />
+          {stockMessage}
+        </p>
 
         <button onClick={()=> onClickingEdit(keg.id) }>
           Edit Keg
