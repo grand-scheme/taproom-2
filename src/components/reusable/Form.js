af6e0aa4bc60292400101951004f153bf9be7597
Shannon Grantski <grantski@pm.me>
2021-02-17 01:16:33 -0800

    add labels

--- src/components/reusable/Form.js
+++ src/components/reusable/Form.js
@@ -5,37 +5,41 @@ import { v4 } from "uuid";
 function Form(props){
   return(
     <>
-      <form onSubmit={props.formSubmissionHandler}>        
+      <form onSubmit={props.formSubmissionHandler}>
         <input 
           type='hidden'
           name="id"
           value= {v4()}
         />
-        <br />
+        <label for="name">Brew Name: </label>
         <input 
           type='text'
           name="name"
           placeholder="Brew Name"
         />
         <br />
+        <label for="brandName">Brewing Company Brand Name: </label>
         <input 
           type='text'
           name="brandName"
           placeholder="Brand Name"
         />
         <br />
+        <label for="price">$: </label>
         <input 
           type='text'
           name="price"
           placeholder="Price per Pint"
         />
         <br />
+        <label for="abv">ABV: </label>
         <input 
           type='text'
           name="abv"
           placeholder="Alcohol by Volume"
         />
         <br />
+        <label for="inventory">Inventory In Stock: </label>
         <input 
           type='text'
           name="inventory"
