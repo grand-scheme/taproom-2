20283e1656571fd1a0a3f665456d728f365596c9
Shannon Grantski <grantski@pm.me>
2021-02-17 00:46:28 -0800

    add linebreaks between input fields

--- src/components/reusable/Form.js
+++ src/components/reusable/Form.js
@@ -11,31 +11,37 @@ function Form(props){
           name="id"
           value= {v4()}
         />
+        <br />
         <input 
           type='text'
           name="name"
           placeholder="Brew Name"
         />
+        <br />
         <input 
           type='text'
           name="brandName"
           placeholder="Brand Name"
         />
+        <br />
         <input 
           type='text'
           name="price"
           placeholder="Price per Pint"
         />
+        <br />
         <input 
           type='text'
           name="abv"
           placeholder="Alcohol by Volume"
         />
+        <br />
         <input 
           type='text'
           name="inventory"
           placeholder="Pints In Keg"
         />
+        <br />
         <button type="submit"> {props.buttonText} </button>
       </form>
     </>
