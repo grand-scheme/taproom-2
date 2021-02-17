5500e012fadd60dde249b9fe4232cfc13288704b
Shannon Grantski <grantski@pm.me>
2021-02-17 01:15:08 -0800

    change header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(){
   return(
     <>
-      <h1>Header placeholder</h1>
+      <h1>This is where the bar's name would go... if it had one!!</h1>
     </>
   );
 }
