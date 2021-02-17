14c74a06046b14feefdc26ea224ce9e5fec4d398
Shannon Grantski <grantski@pm.me>
2021-02-17 00:26:30 -0800

    adjust spacing for legibility

--- src/components/KegController.js
+++ src/components/KegController.js
@@ -3,7 +3,7 @@ import AddNewKeg from './AddNewKeg';
 import KegList from './KegList';
 import KegDetail from './KegDetail';
 import ModifyKegList from './ModifyKegList';
-import {kegInventory} from '../data/kegs'
+import {kegInventory} from '../data/kegs';
 
 class KegController extends React.Component{
   constructor(props) {
@@ -38,8 +38,7 @@ class KegController extends React.Component{
     if (this.state.selectedKeg.inventory > 0) {
       this.state.selectedKeg.inventory--;
     }
-    this.setState({
-    });
+    this.setState({ });
   }
 
   handleAddKegToList = (newKeg) => {
@@ -76,39 +75,44 @@ class KegController extends React.Component{
     });
   }
 
-
-
-
-///////////////////////////////////////
+////// NOTE: this marks the end of state handlers. render method follows this point. //////
   render(){
     let currentVisibleState = null;
     let addKegBtnText = null;
 
     if (this.state.visibleEditKegForm) {
-      currentVisibleState = <ModifyKegList
-        keg = {this.state.selectedKeg}
-        onUpdateKeg = {this.handleEditKeg}
+      currentVisibleState = 
+        <ModifyKegList
+          keg = {this.state.selectedKeg}
+          onUpdateKeg = {this.handleEditKeg}
         />
         addKegBtnText = "View Kegs on Tap"
     } else if (this.state.selectedKeg !=null) {
       currentVisibleState = 
-      <KegDetail 
-        keg = {this.state.selectedKeg} 
-        onClickingDelete = {this.handleDeletingKeg}
-        onClickingEdit = {this.handleEditClick}
-        onClickingSell = {this.handleSellClick}
-      />
-      addKegBtnText = "View Kegs on Tap";
+        <KegDetail 
+          keg = {this.state.selectedKeg} 
+          onClickingDelete = {this.handleDeletingKeg}
+          onClickingEdit = {this.handleEditClick}
+          onClickingSell = {this.handleSellClick}
+        />
+        addKegBtnText = "View Kegs on Tap";
     } else if (this.state.visibleNewKegForm) {
-      currentVisibleState = <AddNewKeg onCreateNewKeg={this.handleAddKegToList} />
-      addKegBtnText = "View Kegs on Tap"
+      currentVisibleState = 
+        <AddNewKeg 
+          onCreateNewKeg={this.handleAddKegToList} 
+        />
+        addKegBtnText = "View Kegs on Tap"
     } else {
-      currentVisibleState = <KegList kegList={this.state.kegListAll} onSelectKeg={this.handleChangeSelection} />
-      addKegBtnText = "Add Keg to Inventory"
-        }
+      currentVisibleState = 
+        <KegList 
+          kegList={this.state.kegListAll} 
+          onSelectKeg={this.handleChangeSelection} 
+        />
+        addKegBtnText = "Add Keg to Inventory"
+    }
+    
     return(
       <>
-        <h1>KegController placeholder</h1>
         {currentVisibleState}
         <button onClick={this.handleClick}> 
           {addKegBtnText} 
@@ -118,4 +122,4 @@ class KegController extends React.Component{
   }
 }
 
-export default KegController;
+export default KegController; 
