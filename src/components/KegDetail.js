edfd77e4105e6f5d26003397e283ff18512f3f85
Shannon Grantski <grantski@pm.me>
2021-02-16 23:18:05 -0800

    import keg information from Information

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,31 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Information from "./reusable/Information";
 
 function KegDetail(props){
   const { keg, onClickingDelete, onClickingEdit } = props;
-  
-  let stockMessage = null;
-  const inStock = "Approximately " + keg.inventory + " pints available.";
-  const lowStock = inStock + " Almost Empty!";
-  const outOfStock = "Out of Stock!";
-
-  if (keg.inventory === 0) {
-    stockMessage = outOfStock;
-  } else if (keg.inventory < 10 ) {
-    stockMessage = lowStock;
-  } else {
-    stockMessage = inStock;
-  }
 
   return(
     <>
-      <h1>{keg.name}</h1>
-      <h2>Proudly brewed by {keg.brandName}</h2>
-        <p>
-          {keg.abv}% Alcohol by Volume <br />
-          $ {keg.price} per pint <br />
-          {stockMessage}
-        </p>
+      <Information 
+        entryFlag=""
+        keg={keg}/>
 
         <button onClick={()=> onClickingEdit(keg.id) }>
           Edit Keg
