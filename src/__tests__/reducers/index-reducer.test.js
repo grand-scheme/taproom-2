a7d49a9cf0879a8a54327b9a03551c1e5ec07e1c
Shannon Grantski <grantski@pm.me>
2021-02-24 00:16:46 -0800

    add smoke tests for root reducer

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,5 +1,7 @@
 import rootReducer from '../../reducers/index-reducer';
 import { createStore } from 'redux';
+import kegListReducer from '../../reducers/keg-list-reducer';
+import formVisibilityReducer from '../../reducers/form-visibility-reducer';
 
 let store = createStore(rootReducer);
 
@@ -23,6 +25,74 @@ describe(
         );
       }
     );
-    // end test 1
+    // end test 1 // start test 2
+    test(
+      "Check that initial state of rdxKegListAll matches root", () => {
+        expect(
+          store.getState()
+          .rdxKegListAll
+        ).toEqual(
+          kegListReducer(
+            undefined, {
+              type: null
+            }
+          )
+        )
+      }
+    )
+    // end test 2 // start test 3
+    test(
+      "Check that initial state of rdxFormVisible matches root", () => {
+        expect(
+          store.getState().rdxFormVisible
+        ).toEqual(
+          formVisibilityReducer(
+            undefined, {
+              type: null
+            }
+          )
+        );
+      }
+    );
+    // end test 3 // start test 4
+    test(
+      "Check that ADD_KEG action still works", () => {
+        const action = {
+          type: 'ADD_KEG', 
+          id: 3,
+          name: 'Trois',
+          brandName: 'Thalatha',
+          price: 3,
+          abv: 3,
+          inventory: 3
+        }
+        store.dispatch(action);
+        expect(
+          store.getState()
+          .rdxKegListAll
+        ).toEqual(
+          kegListReducer(
+            undefined, action
+          )
+        );
+      }
+    );
+    // end test 4 // start test 5
+    test(
+      "Check that TOGGLE_FORM still works", () => {
+        const action = {
+          type: "TOGGLE_FORM"
+        }
+        store.dispatch(action);
+        expect(
+          store.getState().rdxFormVisible
+        ).toEqual(
+          formVisibilityReducer(
+            undefined, action
+          )
+        )
+      }
+    )
+    // end test 5
   }
 )
