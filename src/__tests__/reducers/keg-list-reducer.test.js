905c28581435dc3ec50fc5e22426e02c8803526f
Shannon Grantski <grantski@pm.me>
2021-02-23 22:16:21 -0800

    fail third test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,16 +2,34 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
   let action;
-  const kegData = {
-    id: 1,
-    name: "Un",
-    brandName: "Wahid",
-    price: 3,
-    abv: 4,
-    inventory: 5
+  const kegData = { // for test 2
+    id: 0,
+    name: "Zéro",
+    brandName: "Sifr",
+    price: 0,
+    abv: 0,
+    inventory: 0
+  }
+  const testState = { // for test 3
+    1: {
+      id: 1,
+      name: "Un",
+      brandName: "Wahid",
+      price: 1,
+      abv: 1,
+      inventory: 1
+    },
+    2: {
+      id: 2,
+      name: "Deux",
+      brandName: "Ithnan",
+      price: 2,
+      abv: 2,
+      inventory: 2
+    }
   }
 
-
+  // end DESCRIBES
   // start test 1
   test('Should return default state if no actions are taken', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
@@ -29,8 +47,26 @@ describe('kegListReducer', () => {
         id, name, brandName, price, abv, inventory
       }
     });
-
   });
+  // end test 2 // start test 3
+  test('Should delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 1
+    };
+    expect(kegListReducer(testState, action)).toEqual({
+      2: {
+        id: 2,
+        name: "Deux",
+        brandName: "Ithnan",
+        price: 2,
+        abv: 2,
+        inventory: 2
+      }
+    });
+  });
+
+  // end test 3 //
 
 
 
