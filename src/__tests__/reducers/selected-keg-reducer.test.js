3698114aa9a71240f6d1a4ea6291ec2721c1d606
Shannon Grantski <grantski@pm.me>
2021-02-24 05:29:52 -0800

    write second test for selector

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,7 +1,15 @@
 import selectedKegReducer from '../../reducers/selected-keg-reducer';
 
 describe('selectedKegReducer', () => {
-
+  let action;
+  const kegData = { // for test 2
+    id: 5,
+    name: 'Cinq',
+    brandName: 'Khamza',
+    price: 5,
+    abv: 5,
+    inventory: 5
+  }
 
   // end DESCRIBES
 
@@ -11,4 +19,13 @@ describe('selectedKegReducer', () => {
     .toEqual({});
   });
   // end test 1
+
+  // test 2
+  test('Should return the keg specified when specified', () => {
+    action = { type: 'SELECT_KEG',
+      selectedKeg: kegData };
+      expect(selectedKegReducer(null, action))
+      .toEqual(kegData);
+    }
+  )
 })
