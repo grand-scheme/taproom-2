b598930bbb1d7e47c5f8d49580bdcd1ffbeaba81
Shannon Grantski <grantski@pm.me>
2021-02-24 04:12:40 -0800

    remove duplicate import statement

--- src/reducers/index-reducer.js
+++ src/reducers/index-reducer.js
@@ -1,7 +1,6 @@
 import formVisibilityReducer from './form-visibility-reducer';
 import formEditReducer from './form-edit-reducer';
 import kegListReducer from './keg-list-reducer';
-import formEditReducer from './form-edit-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers(
