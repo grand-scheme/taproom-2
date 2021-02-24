84ca304e0e47f6d07c462b2c3575474093dea94d
Shannon Grantski <grantski@pm.me>
2021-02-24 03:00:16 -0800

    add edit reducer

---
+++ src/__tests__/reducers/form-edit-reducer.test.js
@@ -0,0 +1,20 @@
+import formEditReducer from '../../reducers/form-edit-reducer';
+
+describe("formEditReducer", () => {
+
+  // start test 1 //
+test('Should return default state if no action type is recognized', () => {
+    expect(formEditReducer(undefined, { type: null } ))
+    .toEqual(false);
+  });
+  // end test 1 
+
+  // begin test 2
+test("Should toggle form from new to edit (from false to true)", () => {
+    expect(formEditReducer(false, { type: "TOGGLE_EDIT" }))
+    .toEqual(true);
+  });
+  // end test 2
+
+  // end test suite
+});
