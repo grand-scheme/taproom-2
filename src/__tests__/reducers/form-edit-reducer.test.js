0307bcddc001f24ba08bba2aaac5a83a8b6dff40
Shannon Grantski <grantski@pm.me>
2021-02-24 01:15:15 -0800

    pass two tests for edit reducer

--- src/__tests__/reducers/form-edit-reducer.test.js
+++ src/__tests__/reducers/form-edit-reducer.test.js
@@ -14,6 +14,14 @@ describe(
         );
       }
     )
-    // end test 1
+    // end test 1 // start test 2
+    test(
+      "Should toggle new/edit form (from false to true)", () => {
+        expect(
+          formEditReducer(false, { type: "TOGGLE_EDIT" })
+        ).toEqual(true)
+      }
+    )
+    // end test 2
   }
 )
