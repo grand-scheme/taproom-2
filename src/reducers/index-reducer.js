c1f8a2859f9ce1fb8ebb79585153e9fe34e725e8
Shannon Grantski <grantski@pm.me>
2021-02-24 05:53:08 -0800

    pass smoke test

--- src/reducers/index-reducer.js
+++ src/reducers/index-reducer.js
@@ -1,13 +1,15 @@
 import formVisibilityReducer from './form-visibility-reducer';
 import formEditReducer from './form-edit-reducer';
 import kegListReducer from './keg-list-reducer';
+import selectedKegReducer from './selected-keg-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers(
   {
     rdxFormIsVisible: formVisibilityReducer,
     rdxKegListAll: kegListReducer,
-    rdxFormIsEdit: formEditReducer
+    rdxFormIsEdit: formEditReducer,
+    rdxSelectedKeg: selectedKegReducer
   }
 );
 
