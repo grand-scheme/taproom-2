34fb434b536835785fac96242a0d2f2482e16482
Shannon Grantski <grantski@pm.me>
2021-02-23 22:02:30 -0800

    fail second test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,37 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+  let action;
+  const kegData = {
+    id: 1,
+    name: "Un",
+    brandName: "Wahid",
+    price: 3,
+    abv: 4,
+    inventory: 5
+  }
+
+
+  // start test 1
   test('Should return default state if no actions are taken', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
+// end test 1 // start test 2
+  test('Should add new keg to list', () => {
+    const {id, name, brandName, price, abv, inventory} = kegData;
+    action = {
+      type: 'ADD_KEG',
+      id, name, brandName, price, abv, inventory
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        id, name, brandName, price, abv, inventory
+      }
+    });
+
+  });
+
+
+
 });
