70bebc90f4f4d981fc34ca440b5cce2dbc5dffac
Shannon Grantski <grantski@pm.me>
2021-02-23 22:05:34 -0800

    pass second test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,13 @@
 export default (state = {}, action) => {
-  return state;
+  const { id, name, brandName, price, abv, inventory } = action;
+  switch (action.type) {
+    case 'ADD_KEG':
+      return Object.assign({}, state, {
+        [id] : {
+          id, name, brandName, price, abv, inventory
+        }
+      });
+    default:
+      return state;
+  }
 };
