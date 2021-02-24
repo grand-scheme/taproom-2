b6ad7b7311e06928a3ee8271b2ef364d13a68572
Shannon Grantski <grantski@pm.me>
2021-02-24 01:22:35 -0800

    import FER to index reducer, pass test

--- src/reducers/index-reducer.js
+++ src/reducers/index-reducer.js
@@ -1,11 +1,13 @@
 import formVisibilityReducer from './form-visibility-reducer';
 import kegListReducer from './keg-list-reducer';
+import formEditReducer from './form-edit-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers(
   {
     rdxFormVisible: formVisibilityReducer,
-    rdxKegListAll: kegListReducer
+    rdxKegListAll: kegListReducer,
+    formIsEdit: formEditReducer
   }
 );
 
