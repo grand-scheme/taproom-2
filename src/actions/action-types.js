8c2891f4a081feaebe5c13875d72ed38ed1e85c2
Shannon Grantski <grantski@pm.me>
2021-02-24 06:01:05 -0800

    add action types for select/reject keg

--- src/actions/action-types.js
+++ src/actions/action-types.js
@@ -2,3 +2,5 @@ export const DELETE_KEG = 'DELETE_KEG';
 export const TOGGLE_FORM = 'TOGGLE_FORM';
 export const TOGGLE_EDIT = 'TOGGLE_EDIT';
 export const ADD_KEG = 'ADD_KEG';
+export const SELECT_KEG = 'SELECT_KEG';
+export const REJECT_KEG = 'REJECT_KEG';
